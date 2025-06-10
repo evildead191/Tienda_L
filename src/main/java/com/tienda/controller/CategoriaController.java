8d94a9093d1ea9787db9b7e5c6601b3263eed1f5
Laboratorios <Laboratorios@172.16.11.6>
2025-06-09 20:48:47 -0600

    Semana 5, Eliminar y actualizar

--- src/main/java/com/tienda/controller/CategoriaController.java
+++ src/main/java/com/tienda/controller/CategoriaController.java
@@ -1,11 +1,15 @@
 package com.tienda.controller;
 
+import com.tienda.domain.Categoria;
 import com.tienda.service.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("/categoria")
@@ -21,5 +25,28 @@ public class CategoriaController {
         model.addAttribute("categorias", lista);
         
         return "/categoria/listado";
+    }
+    
+    @PostMapping("/guardar")
+    public String guardar(Categoria categoria, 
+            @RequestParam("imagenFile") MultipartFile imagenFile) {
+        
+        categoriaService.save(categoria);
+        return "redirect:/categoria/listado";
+        
+    }
+    
+    @GetMapping("/eliminar/{idCategoria}")
+    public String eliminar(Categoria categoria) {
+        categoriaService.delete(categoria);
+        return "redirect:/categoria/listado";
+        
+    }
+    
+    @GetMapping("/modificar/{idCategoria}")
+    public String modificar(Categoria categoria, Model model) {
+        categoria = categoriaService.getCategorias(categoria);
+        model.addAttribute("categoria", categoria);
+        return "/categoria/modifica";
     }
 }
--- src/main/java/com/tienda/service/CategoriaService.java
+++ src/main/java/com/tienda/service/CategoriaService.java
@@ -9,22 +9,39 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CategoriaService {
-    
+
     //Se usa para crear de forma automática una única instancia de la clase
     @Autowired
     private CategoriaRepository categoriaRepository;
-    
+
     //Se usa para indicar que se hará una transacción a una BD
-    @Transactional(readOnly=true)
-    public List<Categoria> getCategorias(boolean activos){
+    @Transactional(readOnly = true)
+    public List<Categoria> getCategorias(boolean activos) {
         //Se usa "activos" si se desea limitar la vista a solo categorias activas
-        var lista=categoriaRepository.findAll();
-        
+        var lista = categoriaRepository.findAll();
+
         if (activos) { //Solo si se desean los registros de categorias activas
             lista.removeIf(e -> !e.isActivo());
         }
-        
+
         return lista;
     }
+    
+    @Transactional(readOnly = true)
+    public Categoria getCategorias(Categoria categoria) {
+        return categoriaRepository.findById(categoria.getIdCategoria()).orElse(null);
+    }
+
+    //Se usa para indicar que se hará una transacción a una BD
+    @Transactional
+    public void save(Categoria categoria) {
+        categoriaRepository.save(categoria);
+    }
+    
+    @Transactional
+    public void delete(Categoria categoria) {
+        categoriaRepository.delete(categoria);
+    }
+    
     
 }
