8aabb43be5fff84fff6595cd9e26abaa96b67cc3
Laboratorios <Laboratorios@172.16.11.3>
2025-06-30 18:06:20 -0600

    Errores arreglaos

--- src/main/java/com/tienda/domain/Producto.java
+++ src/main/java/com/tienda/domain/Producto.java
@@ -6,7 +6,7 @@ import lombok.Data;
 
 @Data
 @Entity
-@Table(name = "categoria")
+@Table(name = "producto")
 public class Producto implements Serializable {
 
     private static final long serialVersionUID = 1L;
